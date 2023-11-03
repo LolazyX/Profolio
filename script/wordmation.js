776443064e582dcd8c7af1496bb88657e6b3c8bb
LolazyX <arsik.789.king@gmail.com>
2023-11-03 10:28:28 +0700

    update

---
+++ script/wordmation.js
@@ -0,0 +1,112 @@
+const base = [
+    [" "," "," "," "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "," "," "," "],
+]
+
+
+const word = [
+    [
+        ["▶","H","e","l","l","o"," "," "," "," "," "," "," "],
+        [" "," "," "," ","e","v","e","r","y","o","n","e","!"],
+        ["I"," ","a","m"," "," "," "," "," "," "," "," "," "],
+        [" "," "," "," ","L","o","l","a","z","y","X"," "," "],
+    ],
+    [
+        ["I"," ","a","m"," "," "," "," "," "," "," "," "," "],
+        [" "," "," ","i","n","t","e","r","e","s","t","e","d"],
+        [" ","i","n","●","w","e","b"," "," "," "," "," "," "],
+        [" "," ","d","e","v","e","l","o","p","e","r","!"," "],
+    ],
+    [
+        ["▶","Y","o","u"," ","c","a","n"," ","v","i","e","w"],
+        [" "," ","m","y"," ","w","o","r","k"," "," "," "," "],
+        [" "," "," "," "," "," "," "," "," ","o","n"," "," "],
+        [" "," "," "," "," "," ","★","G","i","t","H","u","b"],
+    ],
+    [
+        ["▶","a","n","d"," ","y","o","u"," ","c","a","n"," "],
+        [" "," ","c","o","n","t","a","c","t"," ","m","e"," "],
+        ["w","h","e","n","e","v","e","r"," "," "," "," "," "],
+        [" "," "," "," ","y","o","u"," ","n","e","e","d","!"],
+    ]
+
+]
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const randomCharecter = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let counter = 0;
+    while (counter < 1) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      counter += 1;
+    }
+    return result;
+}
+
+const randomColor = () => {
+    const color = ['infra-red', 'gold', 'caribbean-green'];
+    let result = color[Math.floor((Math.random()*color.length))];
+    return result;
+}
+
+const clearClass = () => { 
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < word[i].length; j++) {
+            let letter;
+            j == 0 ? letter = 'a' : j == 1 ? letter = 'b' : j == 2 ? letter = 'c' : j == 3 ? letter = 'd': j = 4;
+            for (let k = 0; k < word[i][j].length; k++) {
+                let id = `${letter}${k+1}`;
+                document.getElementById(id).classList.add("border-opacity-rgba");
+                document.getElementById(id).classList.remove("shadow-inner");
+                document.getElementById(id).classList.remove("border-charcoal");
+                document.getElementById(id).classList.remove("text-infra-red");
+                document.getElementById(id).classList.remove("text-gold");
+                document.getElementById(id).classList.remove("text-caribbean-green");
+                document.getElementById(id).innerHTML = "ㅤ";
+            }
+        }
+    }
+}
+
+const looptest = async () => { 
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < word[i].length; j++) {
+            let letter;
+            j == 0 ? letter = 'a' : j == 1 ? letter = 'b' : j == 2 ? letter = 'c' : j == 3 ? letter = 'd': j = 4;
+            for (let k = 0; k < word[i][j].length; k++) {
+                if (word[i][j][k] != " ") {
+                    let id = `${letter}${k+1}`;
+                    document.getElementById(id).classList.remove("border-opacity-rgba");
+                    document.getElementById(id).classList.add("shadow-inner");
+                    document.getElementById(id).classList.add("border-charcoal");
+                    document.getElementById(id).classList.add("text-charcoal");
+                    document.getElementById(id).innerHTML = randomCharecter();
+                    await sleep(60)
+                    document.getElementById(id).innerHTML = randomCharecter();
+                    await sleep(60)
+                    document.getElementById(id).innerHTML = randomCharecter();
+                    await sleep(60)
+                    document.getElementById(id).classList.remove("text-charcoal");
+                    if (word[i][j][k] == "▶" || word[i][j][k] == "!" || word[i][j][k] == "X" || word[i][j][k] == "★" || word[i][j][k] == "●") {
+                        let color = `text-${randomColor()}`;
+                        document.getElementById(id).classList.add(color);
+                    }
+                    document.getElementById(id).innerHTML = word[i][j][k];
+                    await sleep(20)
+                }
+            }
+        }
+        await sleep(5000);
+        clearClass();
+        console.log(i);
+        if ( i == (word.length - 1) ) i = -1;
+    }
+}
+
+looptest();
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                'rich-black' : '#000814',
+                'dark-jungle-green' : '#161E29',
+                'charcoal' : '#485E80',
+                'golden-poppy' : '#ffc300',
+                'gold' : '#ffd60a',
+                'infra-red' : '#EF476F',
+                'infra-red2' : '#631E2E',
+                'caribbean-green' : '#06D6A0',
+                'lavender-gray' : '#BFC7D2',
+                'crayola': '#A6AEB9',
+                'space-light': '#536878',
+                'opacity-rgba': 'rgba(0, 0, 0, 0)'
+            },
+            fontFamily: {
+                'kanit': ['Kanit', 'sans-serif']
+            },
+        },
+    },
+    plugins: [],
+}
