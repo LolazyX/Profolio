6cddf5bf062a8de92be7e1379261a6b642eabd5b
LolazyX <arsik.789.king@gmail.com>
2023-11-04 22:54:31 +0700

    update

--- script/app.js
+++ script/app.js
@@ -6,14 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (title_page) {
             const title_pageRect = title_page.getBoundingClientRect();
             if (title_pageRect.top <= 0) {
-                logo.classList.remove('text-infra-red');
-                logo.classList.add('text-white')
                 navbar.classList.add('bg-rich-black');
                 navbar.classList.add('border-b-2')
                 navbar.classList.add('border-infra-red')
             } else {
-                logo.classList.add('text-infra-red');
-                logo.classList.remove('text-white')
                 navbar.classList.remove('bg-rich-black');
                 navbar.classList.remove('border-b-2')
                 navbar.classList.remove('border-infra-red')
